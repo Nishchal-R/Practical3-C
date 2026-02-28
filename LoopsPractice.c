00b90255e59663153e534f6a89bc7ef3140ff5ac
Nishchal Ranjitkar <Nishchal-R@users.noreply.github.com>
2026-02-28 15:27:49 +0545

    Practical 3 part-3 exercise 1a completed

---
+++ LoopsPractice.c
@@ -0,0 +1,12 @@
+/*Electricity bill
+Practical 3, Part 3
+@Nishchal Ranjitkar*/
+
+#include<stdio.h>
+int main(){
+    for(int i= 1;i<= 10;i++){
+        printf("%d: Welcome to C Programming!\n", i);
+    }
+
+    return 0;
+}
